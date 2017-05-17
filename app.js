0477e4b4378bb56a3115a49447c866ffbcc260c2
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-05-17 15:01:03 +0100

    adds initial front end functionality

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ejs = require('ejs');
 
 // Set up the express app
 const app = express();
+var session = require('express-session');
+var sess;
 
 // Log requests to the console.
 app.use(logger('dev'));
@@ -13,16 +15,40 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//adds css fucntionality
+app.use(express.static(__dirname + '/public'));
+
+//----------------------------------------------------------------
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
+}))
+
 //----------------------------------------------------------------
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render("home",{Error:"none"});
+  res.render("home");
+});
+
+app.get('/signup', function(req, res) {
+  res.render("signup");
+});
+
+app.get('/login', function(req, res) {
+  res.render("login");
 });
 
 app.post('/confirmation', function(req, res) {
-  res.render("confirmation", {Error:"none"});
+  sess = req.session;
+  sess.name = req.body.name;
+  res.render("confirmation", {
+    name: sess.name
+  });
 });
 
 //----------------------------------------------------------------
