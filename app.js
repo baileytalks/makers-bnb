159ec3f90ac1bef3856f9d00510a8ba5f9e994af
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-05-18 17:50:25 +0100

    first commit

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const usersController = require('./server/controllers/users.js');
 
 // Set up the express app
 const app = express();
 var session = require('express-session');
 var sess;
 
+var Sequelize = require('sequelize');
+
+var connection = new Sequelize('makers_bnb_development', 'postgres', 'password', {
+  dialect: 'postgres'
+});
+
+var User = connection.define ('user', {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+});
+
+var Property = connection.define ('property', {
+  title: Sequelize.STRING,
+  description: Sequelize.STRING,
+  price: Sequelize.STRING,
+});
 // Log requests to the console.
 app.use(logger('dev'));
 
@@ -54,12 +70,50 @@ app.post('/confirmation', function(req, res) {
   sess.password = req.body.password;
   sess.passwordConfirmation = req.body.passwordConfirmation;
 
- passwordConfirmation(res);
+  connection.sync().then(function() {
+    User.create({
+      name: sess.name,
+      email: sess.email,
+      password: sess.password,
+    })
+  });
 
- usersController.create(sess.name, sess.email, sess.password);
+ passwordConfirmation(res);
 
  res.render("confirmation", {
     name: sess.name
+  })
+})
+
+app.get('/property', function(req, res) {
+  res.render("newProperty");
+});
+
+app.post('/newProperty', function(req, res) {
+  sess = req.session;
+  sess.title = req.body.title;
+  sess.description = req.body.description;
+  sess.price = req.body.price;
+
+  connection.sync().then(function() {
+    Property.create({
+      title: sess.title,
+      description: sess.description,
+      price: sess.price,
+    })
+  })
+
+  res.redirect("/propertyList");
+})
+
+app.get('/propertyList', function(req, res) {
+  connection.sync().then(function() {
+    (Property.findAll().then( function(users) {users = users})
+
+  })
+
+  res.render('propertyList', {
+    users: users
   })
 })
 
